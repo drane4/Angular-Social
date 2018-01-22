a051237cafb07713989d562233a4cb97468e79d2
Menard Gardose <mjgardose@gmail.com>
2018-01-22 17:31:35 +0800

    add environments folder

---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+  	apiKey: "AIzaSyDBO28FTYtleIWU0V7ejsivS1R7GgBftO8",
+    authDomain: "firestore-crud-of-users-2c81a.firebaseapp.com",
+    databaseURL: "https://firestore-crud-of-users-2c81a.firebaseio.com",
+    projectId: "firestore-crud-of-users-2c81a",
+    storageBucket: "firestore-crud-of-users-2c81a.appspot.com",
+    messagingSenderId: "960666650052"
+
+  }
+};
+
